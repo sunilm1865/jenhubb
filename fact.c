0d6a7e5dd82dd58077cdaa8a2ce0d49bc44010e0
Cloud User <ec2-user@ip-172-31-42-251.us-east-2.compute.internal>
2018-09-22 18:16:32 +0000

    added and m

---
+++ fact.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+fact(){
+  int i,f=1,num;
+ 
+  printf("Enter a number: ");
+  scanf("%d",&num);
+ 
+  for(i=1;i<=num;i++)
+      f=f*i;
+ 
+  printf("Factorial of %d is: %d",num,f);
+}
